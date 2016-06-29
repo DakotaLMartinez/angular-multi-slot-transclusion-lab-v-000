572f3465c76fec92a012073daef5c3726e92beb6
Dakota Martinez <dakotaleemusic@gmail.com>
2016-06-29 02:27:14 +0000

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,15 +1,23 @@
+angular
+	.module('app')
+	.directive('userProfile', UserProfile);
+	
 function UserProfile() {
 	return {
-		transclude: true,
+		transclude: {
+			name: 'h4', 
+			position: '?h6', 
+			description: '?p'
+		},
 		template: [
 			'<div>',
 				'<h3>User Profile</h3>',
-				'<div ng-transclude></div>',
+				'<div>Name: <span ng-transclude="name"></span></div>',
+				'<div>Position: <span ng-transclude="position">No Position</span></div>', 
+				'<div>Description:</div>', 
+				'<span ng-transclude="description">No Description</span>',
 			'</div>'
 		].join('')
 	}
 }
 
-angular
-	.module('app')
-	.directive('userProfile', UserProfile);
